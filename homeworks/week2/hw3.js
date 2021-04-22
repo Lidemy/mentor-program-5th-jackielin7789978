465d393b19b3afb841e0df98c9a305c03e0fdd36
jackie <jackielin7789978@gmail.com>
2021-04-22 14:48:00 +0800

    finish hw3, hw4 & hw5

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,10 @@
 function reverse(str) {
-  
+    var arr = str.split('');
+    var reversedArr = [];
+    for (var i = 1; i<=arr.length; i++){
+        reversedArr[arr.length-i] = arr[i-1];
+    } 
+    console.log(reversedArr.join(''));
 }
 
 reverse('hello');
