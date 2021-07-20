404ed1fe67aa74957b9a54a3f30af83ba664bb47
jackie <jackielin7789978@gmail.com>
2021-07-20 10:15:39 +0800

    wrap API calls into functions

--- homeworks/week12/hw1/utils.js
+++ homeworks/week12/hw1/utils.js
@@ -12,20 +12,20 @@ function getComments(site, lastID, cb) {
       const { comments } = resp
       for (const comment of comments) {
         $('.cards').append(`
-        <div class="container mt-4">
-          <div class="row">
-            <div class="col-md-8 offset-md-2">
-            <div class="card">
-              <div class="card-header">
-                @${escapeXSS(comment.nickname)}
+          <div class="container mt-4">
+            <div class="row">
+              <div class="col-md-8 offset-md-2">
+              <div class="card">
+                <div class="card-header">
+                  @${escapeXSS(comment.nickname)}
+                </div>
+                <div class="card-body">
+                  <p class="card-text break">${escapeXSS(comment.content)}</p>
+                </div>
               </div>
-              <div class="card-body">
-                <p class="card-text break">${escapeXSS(comment.content)}</p>
               </div>
-            </div>
             </div>
           </div>
-        </div>
         `)
       }
       cb(comments)
@@ -48,6 +48,38 @@ function getTotalComments(site, cb) {
     }
   })
 }
+
+function addComment(commentData) {
+  $.ajax({
+    type: 'POST',
+    url: 'http://mentor-program.co/mtr04group1/jackie/week12/hw1/api_add_comment.php',
+    data: commentData
+  }).done((data) => {
+    if (!data.ok) {
+      alert('暱稱和留言不得留空')
+      return
+    }
+    $('#nickname').val('')
+    $('#textarea').val('')
+    $('.cards').prepend(`
+      <div class="container mt-4">
+        <div class="row">
+          <div class="col-md-8 offset-md-2">
+          <div class="card">
+            <div class="card-header">
+              @${escapeXSS(commentData.nickname)}
+            </div>
+            <div class="card-body">
+              <p class="card-text break">${escapeXSS(commentData.content)}</p>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    `)
+  })
+}
+
 function escapeXSS(str) {
   return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#039;')
 }
