6188ff88582d0707cc61da49841ec24010817ae6
jackie <jackielin7789978@gmail.com>
2021-08-09 20:58:57 +0800

    revise week13 homeworks

--- homeworks/week13/hw3/script.js
+++ homeworks/week13/hw3/script.js
@@ -1,21 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const BASE_URL = 'https://api.twitch.tv/kraken'
   const CLIENT_ID = 'vvwxov0mlu750xfx45nuirctqnmwbr'
-  const streamTemplate = `
-    <a href="$URL" target="_blank">
-      <div class="stream">
-        <img src="$preview" alt="stream preview">
-        <div class="stream__data">
-          <div class="stream__data__avatar">
-            <img src="$avatar" alt="channel logo">
-          </div>
-          <div class="stream__data__info">
-            <div class="stream__data__info__status">$status</div>
-            <div class="stream__data__info__channel">$name</div>
-          </div>
-        </div>
-      </div>
-    </a>`
+
   // 先抓到前五個遊戲，把遊戲名稱 append 到選單上並渲染第一個遊戲的實況
   getGames(`${BASE_URL}/games/top?limit=5`, CLIENT_ID)
 
@@ -24,17 +10,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const target = e.target.innerText
     const currentGame = document.querySelector('h1').innerText
     if (target !== currentGame) {
-      changeGame(e.target.innerText)
+      changeGame(target)
     }
   })
   // 點擊「載入更多」即可呼叫 changeGame 改變實況頁面
+  let isClick = true
   document.querySelector('.more-btn').addEventListener('click', () => {
-    const gamename = document.querySelector('h1').innerText
-    const currentStreamsNum = document.querySelectorAll('.stream').length
-    if (currentStreamsNum > 900) {
-      document.querySelector('.more-btn').classList.add('hide')
-    } else {
-      getStreams(`${BASE_URL}/streams/?game=${gamename}&limit=20&offset=${currentStreamsNum}`, CLIENT_ID)
+    if (isClick) {
+      isClick = false
+      const gamename = document.querySelector('h1').innerText
+      const currentStreamsNum = document.querySelectorAll('.stream').length
+      if (currentStreamsNum > 900) {
+        document.querySelector('.more-btn').classList.add('hide')
+      } else {
+        getStreams(`${BASE_URL}/streams/?game=${gamename}&limit=20&offset=${currentStreamsNum}`, CLIENT_ID)
+      }
+      setTimeout(() => {
+        isClick = true
+      }, 1000)
     }
   })
   // funciton 區
@@ -78,12 +71,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const a = document.createElement('a')
     const { url, logo, status, display_name: displayName } = stream.channel
     const { preview } = stream
-    a.innerHTML = streamTemplate
-      .replace('$URL', url)
-      .replace('$preview', preview.medium)
-      .replace('$avatar', logo)
-      .replace('$status', status)
-      .replace('$name', displayName)
+    a.innerHTML = `
+      <a href="${url}" target="_blank">
+        <div class="stream">
+          <img src="${preview.medium}" alt="stream preview">
+          <div class="stream__data">
+            <div class="stream__data__avatar">
+              <img src="${logo}" alt="channel logo">
+            </div>
+            <div class="stream__data__info">
+              <div class="stream__data__info__status">${status}</div>
+              <div class="stream__data__info__channel">${displayName}}</div>
+            </div>
+          </div>
+        </div>
+      </a>
+    `
     document.querySelector('.streams').append(a)
   }
   function changeGame(gamename) {
